88bf07c5eb60b8df6478ec95c0f386e614c5f98c
Mohammed La <mohammed.laghzaoui@outlook.com>
2018-05-11 00:05:31 +0200

    Workout maker exercises filtered through input of the user

--- src/pages/workout-maker/workout-maker.ts
+++ src/pages/workout-maker/workout-maker.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { WgerProvider, IexerciseCategory } from '../../providers/wger/wger';
 
 
 /**
@@ -16,35 +17,72 @@ import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angu
 })
 export class WorkoutMakerPage {
 
-  testCheckboxResult: any;
-  testCheckboxOpen: boolean;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+  categorys: IexerciseCategory;
+  categoryCheckboxResult: any[];
+  categoryCheckboxOpen: boolean;
+  hypermediaExercises: string
+  
+  constructor(public navCtrl: NavController,public restProvider: WgerProvider, public navParams: NavParams, public alertCtrl: AlertController) {
+    
+    let hypermediaCategorys: string;
+    
+    
+    this.restProvider.getMainContent().subscribe(result =>{
+      hypermediaCategorys = result.exercisecategory
+      this.hypermediaExercises = result.exercise
+      
+      this.getCategorys(hypermediaCategorys);
+      
+    })
+  }
+  
+  getExercises(link: string) {
+    for (var i = 1; i < 27; i++){
+      this.restProvider.getExercises(link+"/?page=" + i).subscribe(result => {
+        //debugger
+        for (let j in result.results){
+
+          if(this.categoryCheckboxResult.find(x => x == result.results[j].category) ){
+
+            console.log(result.results[j].name)
+            
+          }
+        }
+      })
+    }
+  }
+
+  getCategorys(link: string) {
+    this.restProvider.getexerciseCategory(link).subscribe(result =>{
+      this.categorys = result
+
+    
+    })
   }
 
   showCategory() {
     let alert = this.alertCtrl.create();
     alert.setTitle('What do you want to train today?');
 
-    alert.addInput({
-      type: 'checkbox',
-      label: 'Alderaan',
-      value: 'value1',
-      checked: true
-    });
-
-    alert.addInput({
-      type: 'checkbox',
-      label: 'Bespin',
-      value: 'value2'
-    });
-
+    for (let i in this.categorys.results){
+      let categoryName = this.categorys.results[i].name
+      let id = this.categorys.results[i].id
+      alert.addInput({
+        type: 'checkbox',
+        label: categoryName,
+        value: String(id),
+      });
+    }
+  
     alert.addButton('Cancel');
     alert.addButton({
       text: 'Make',
       handler: data => {
         console.log('Checkbox data:', data);
-        this.testCheckboxOpen = false;
-        this.testCheckboxResult = data;
+        this.categoryCheckboxOpen = false;
+        this.categoryCheckboxResult = data;
+        console.log(data)
+        this.getExercises(this.hypermediaExercises);
       }
     });
     alert.present();
