023e32826a81bd35f9be5af9491609d5e3593d71
Mohammed La <mohammed.laghzaoui@outlook.com>
2018-05-11 14:43:32 +0200

    workout maker issue solved and improved when getting data

--- src/pages/workout-maker/workout-maker.ts
+++ src/pages/workout-maker/workout-maker.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
 import { WgerProvider, IexerciseCategory } from '../../providers/wger/wger';
+import { ExerciseDetailsPage } from '../exercise-details/exercise-details';
 
 
 /**
@@ -17,6 +18,7 @@ import { WgerProvider, IexerciseCategory } from '../../providers/wger/wger';
 })
 export class WorkoutMakerPage {
 
+  loading: any;
   categorys: IexerciseCategory;
   categoryCheckboxResult: any[];
   categoryCheckboxOpen: boolean;
@@ -24,8 +26,15 @@ export class WorkoutMakerPage {
   fullWorkoutExercises: any[]
   workoutMaked: string[]
   workoutLength: number
+  id_exercises: number[]
+  nameExercises: string[]
 
-  constructor(public navCtrl: NavController, public restProvider: WgerProvider, public navParams: NavParams, public alertCtrl: AlertController) {
+  constructor(
+    public navCtrl: NavController, 
+    public restProvider: WgerProvider, 
+    public navParams: NavParams, 
+    public alertCtrl: AlertController,
+    private loadingCtrl: LoadingController) {
 
     let hypermediaCategorys: string;
 
@@ -41,6 +50,8 @@ export class WorkoutMakerPage {
 
   getExercises(link: string) {
     let workout = new Array()
+    let id = new Array()
+    let name = new Array()
     for (var i = 1; i < 27; i++) {
       this.restProvider.getExercises(link + "/?page=" + i).subscribe(result => {
         //debugger
@@ -48,10 +59,16 @@ export class WorkoutMakerPage {
 
           if (this.categoryCheckboxResult.find(x => x == result.results[j].category
             && result.results[j].language == 2
-            && result.results[j].name != '')) {
+            && result.results[j].name != '')
+            && result.results[j].name != 'Test') {
 
             workout.push(result.results[j].name)
+            id.push(result.results[j].id)
+            name.push(result.results[j].name)
+
             this.fullWorkoutExercises = workout
+            this.id_exercises = id
+            this.nameExercises = name
 
           }
         }
@@ -62,18 +79,21 @@ export class WorkoutMakerPage {
   makeWorkout() {
     let workout = new Array()
     let num
+    let length = this.fullWorkoutExercises.length
 
     for (var i = 0; i < 10; i++) {
 
-      num = this.getRandomInt(50)
+      num = this.getRandomInt(length)
 
       workout.push(this.fullWorkoutExercises[num])
 
       this.workoutMaked = workout
-      console.log(this.workoutLength)
+      console.log(length)
 
     }
   }
+
+  
 
   getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
@@ -85,6 +105,21 @@ export class WorkoutMakerPage {
     })
   }
 
+  getWorkout() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...',
+      dismissOnPageChange: true
+    });
+    this.loading.present();
+    
+    setTimeout( () => { 
+      this.makeWorkout()
+      this.loading.dismiss() 
+    }, 2000 );
+    
+  }
+
+  
   showCategory() {
     let alert = this.alertCtrl.create();
     alert.setTitle('What do you want to train today?');
@@ -98,6 +133,8 @@ export class WorkoutMakerPage {
         value: String(id),
       });
     }
+  
+    
 
     alert.addButton('Cancel');
     alert.addButton({
@@ -108,10 +145,32 @@ export class WorkoutMakerPage {
         this.categoryCheckboxResult = data;
 
         this.getExercises(this.hypermediaExercises);
-        this.makeWorkout()
-      }
+        this.getWorkout()
+      }  
     });
     alert.present();
   }
 
+  itemSelected(name: any) {
+    let index: number = this.nameExercises.findIndex(y => Object.is(name, y));
+    let id_ex
+
+    for (var i = 0; i < this.id_exercises.length; i++) {
+      if (i == index) {
+        console.log(this.id_exercises[i]);
+        id_ex = this.id_exercises[i]
+      }
+    }
+
+    this.navCtrl.push(ExerciseDetailsPage, {
+      id_ex,
+      name
+    })
+    console.log(name)
+    console.log(index)
+  }
+  
+
 }
+
+
